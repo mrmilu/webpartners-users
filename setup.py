47ba547c7b8cad131eb6b20d1a0becee6f7a16da
Jaime Herencia <jherencia@mrmilu.com>
2019-04-24 20:02:55 +0200

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 
 from setuptools import setup
 
+
 def get_packages(package):
     """
     Return root package and all sub-packages.
@@ -10,18 +11,38 @@ def get_packages(package):
             for dirpath, dirnames, filenames in os.walk(package)
             if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
-setup(name='webpartners-users',
-      version='0.1.5',
-      description='Web Partners base user package',
-      url='https://github.com/webpartners/webpartners-users',
-      author='Jaime Herencia',
-      author_email='jherencia@webpartners.es',
-      license='MIT',
-      packages=get_packages('webpartners_users'),
-      install_requires=[
-          'django',
-          'djangorestframework',
-          'djangorestframework-jwt',
-      ],
-      zip_safe=False
+
+setup(
+    name='webpartners-users',
+    version='0.2.0',
+    description='Web Partners base user package',
+    url='https://github.com/mrmilu/webpartners-users',
+    author='Jaime Herencia',
+    author_email='jherencia@mrmilu.com',
+    license='MIT',
+    packages=get_packages('webpartners_users'),
+    install_requires=[
+        'django',
+        'djangorestframework',
+        'djangorestframework-jwt',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    python_requires='>=3.6',
+    zip_safe=False
 )
